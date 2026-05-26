6174583bab69cd9a19e660084e9323f25e8f56dd
Bellaxata <bealmineey@gmail.com>
2026-05-26 16:18:15 +0700

    Clean file: key.js

--- key.js
+++
@@ -1,9 +0,0 @@
-{
-  "secretCode": "ZyroExp09",
-  "description": "PREMIUM KEY", 
-  "lastUpdated": "20-01-2025",
-  "author": "Zyro Expay", 
-  "status": "Active",
-  "license": "MIT",
-  "version": "1.0.0"
-}
